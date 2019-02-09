cb35f40955675fc9fd6c5047b1f221e8c46a00a5
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-09 15:47:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function add2(n) {
  
 }
 
-var funkyFunction = function()  {
-  return function()
+var funkyFunction = function outerFunction()  {
+  return function innerFunction()
  {return "FUNKY!"
  }
 }
@@ -31,4 +31,4 @@ var funkyFunction = function()  {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
-var theFunk = funkyFunction()
+var theFunk = outerFunction()
