5802677b79ec2a02eef45f62beea5c97a04fd04a
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-09 15:33:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function add2(n) {
  
 }
 
-var funkyFunction = function() {return function() {
+var funkyFunction = function funkyFunction()  {
     return "FUNKY!"
-  }
+  
 
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
