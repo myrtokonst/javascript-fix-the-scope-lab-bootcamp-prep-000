b5651480a76de0f4942279b2b3adf4a372656a18
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-09 15:34:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function add2(n) {
  
 }
 
-var funkyFunction = function()  {{return function()
-    return "FUNKY!"}}
+var funkyFunction = function()  {return function()
+    {return "FUNKY!"}}
   
 
 
